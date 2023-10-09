717b377e594a301958a09ecba671109216150364
InfinityFit <147361734+InfinityFit@users.noreply.github.com>
2023-10-09 14:31:11 +0700

    Update CORS Headers

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin");
     res.setHeader("Access-Control-Allow-Credentials", true);
     next();
 });
