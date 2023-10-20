c0ccac78751a13fce030022d33d2743e22c33f8f
Ruchida Pithaksiripan <im@fai.in.th>
2023-10-20 22:42:38 +0700

    update for google login

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
-import mongoose from 'mongoose';
+import session from 'express-session';
+import passport from "passport";
+import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import cors from "cors";
+import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo';
 import activitiesRouter from "./routes/activities.js";
 import usersRouter from './routes/users.js';
+import socialRouter from "./routes/auth.js";
+
 dotenv.config();
 
 // Set up Express app
@@ -21,13 +27,34 @@ mongoose
     .catch(err => console.error(err));
 
 // Middleware
-app.use(express.json());
-app.use(morgan("common"));
-app.use(helmet());
+app.use(morgan("dev"));
+app.use(helmet({
+    contentSecurityPolicy: false,
+    referrerPolicy: { policy: 'no-referrer-when-downgrade' },
+}));
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/activities', activitiesRouter);
+// Set up session management
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ 
+        client: mongoose.connection.getClient(),
+        collection: 'sessions',
+    })
+}));
+  
+// Initialize Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Set up routes
+app.use('/auth', socialRouter);
 app.use('/users', usersRouter);
+app.use('/activities', activitiesRouter);
 
 // Start server
 app.listen(PORT, () => {
